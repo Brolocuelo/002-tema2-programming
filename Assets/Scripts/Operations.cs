eb0710f6463c22238eadd7004bcb70d238ce26c9
Brolocuelo <luisalfonsorello@gmail.com>
2022-10-18 12:53:50 +0200

    Operations2

--- Assets/Scripts/Operations.cs
+++ Assets/Scripts/Operations.cs
@@ -4,24 +4,49 @@ using UnityEngine;
 
 public class Operations : MonoBehaviour
 {
-    public int x = 7;
-    public float y = 4;
-    public int z = 4;
-    private float result;
+    /* public int x = 7;
+     public float y = 4;
+     public int z = 4;
+     private float result;
+
+     private void Start()
+     {
+         result = x + y;
+         Debug.Log(string.Format("Sum: {0} + {1} = {2}", x, y, result));
+         result = x - y;
+         Debug.Log(string.Format("Subtractions: {0} - {1} = {2}", x, y, result));
+         result = x * y;
+         Debug.Log(string.Format("Product: {0} * {1} = {2}", x, y, result));
+         result = x / y;
+         Debug.Log(string.Format("Division: {0} / {1} = {2}", x, y, result));
+         result = x % z;
+         Debug.Log(string.Format("Module: {0} % {1} = {2}", x, z, result));
+     }*/
+
+    /* public int x = 10;
+     public int y = 4;
+    */
+    private bool areEqual;
+   
+    public string name1 = "Eustaquio";
+    public string name2 = "Caledonio";
+    private bool areDifferent;
+
+    public float x = 2.0f;
+    public float y = 0.5f;
+    private bool isGreater;
+
 
     private void Start()
     {
-        result = x + y;
-        Debug.Log(string.Format("Sum: {0} + {1} = {2}", x, y, result));
-        result = x - y;
-        Debug.Log(string.Format("Subtractions: {0} - {1} = {2}", x, y, result));
-        result = x * y;
-        Debug.Log(string.Format("Product: {0} * {1} = {2}", x, y, result));
-        result = x / y;
-        Debug.Log(string.Format("Division: {0} / {1} = {2}", x, y, result));
-        result = x % z;
-        Debug.Log(string.Format("Module: {0} % {1} = {2}", x, z, result));
+        areEqual = x == y;
+        Debug.Log(areEqual);
+
+        areDifferent = name1 != name2;
+        Debug.Log(areDifferent);
+
+        isGreater = x > y;
+        Debug.Log(isGreater);
     }
-  
  
 }
